d2937e059ecb294ee3905ead3d67e8e623d798ed
Vaishnavi2058 <Vaishnavi.J@revappayyaitservices.com>
2026-02-12 15:34:45 +0530

    i hav did integration on employee on dashboard

--- src/api/employeeApi/EmployeeDashboardApi.js
+++ src/api/employeeApi/EmployeeDashboardApi.js
@@ -1,5 +1,26 @@
-﻿import axiosInstance from "../axiosInstance";
+﻿
+import axiosInstance from "../axiosInstance";
 
-const employeeDashboardApi = {};
+const employeeDashboardApi = {
+  // Dashboard Summary
+  getDashboard: async () => {
+    const response = await axiosInstance.get("/employee/dashboard");
+    return response.data;
+  },
+
+  // Attendance Calendar
+  getAttendance: async (month) => {
+    const response = await axiosInstance.get(
+      `/employee/attendance?month=${month}`
+    );
+    return response.data;
+  },
+  // Announcements
+  getAnnouncements: async () => {
+    const response = await axiosInstance.get("/employee/announcements");
+    return response.data;
+  },
+};
 
 export default employeeDashboardApi;
+
