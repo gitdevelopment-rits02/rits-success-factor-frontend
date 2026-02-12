4625aeff24f8108cc6fa55b6a461f8c581fe9d8d
Kiran2008-m <Kiran.H@revappayyaitservices.com>
2026-02-12 19:42:31 +0530

    i am pushing EmployeeClockMy time page api - Kiran H

--- src/features/Employee/Redux/slices/EmployeeClockMyTimeSlice.js
+++ src/features/Employee/Redux/slices/EmployeeClockMyTimeSlice.js
@@ -12,7 +12,57 @@ const employeeClockMyTimeSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder;
+    builder
+
+      // =========================
+      // CLOCK IN
+      // =========================
+      .addCase(employeeClockMyTimeThunk.clockIn.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(employeeClockMyTimeThunk.clockIn.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(employeeClockMyTimeThunk.clockIn.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Clock In Failed";
+      })
+
+      // =========================
+      // CLOCK OUT
+      // =========================
+      .addCase(employeeClockMyTimeThunk.clockOut.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(employeeClockMyTimeThunk.clockOut.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(employeeClockMyTimeThunk.clockOut.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Clock Out Failed";
+      })
+      // =========================
+// GET CALENDAR
+// =========================
+.addCase(employeeClockMyTimeThunk.getCalendar.pending, (state) => {
+  state.loading = true;
+  state.error = null;
+})
+
+.addCase(employeeClockMyTimeThunk.getCalendar.fulfilled, (state, action) => {
+  state.loading = false;
+  state.data = action.payload;
+})
+
+.addCase(employeeClockMyTimeThunk.getCalendar.rejected, (state, action) => {
+  state.loading = false;
+  state.error = action.payload || "Failed to load calendar";
+});
+
   },
 });
 
