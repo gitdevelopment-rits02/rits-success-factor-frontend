d0679617edafba64ada78e5a9b7928fd956489dc
swati826 <swati.ghante@revappayyaitservices.com>
2026-02-13 17:56:10 +0530

    I integrated on Employee Request Time Off Swati Ghante.

--- src/api/employeeApi/EmployeeRequestTimeOffApi.js
+++ src/api/employeeApi/EmployeeRequestTimeOffApi.js
@@ -1,5 +1,28 @@
 ﻿import axiosInstance from "../axiosInstance";
 
-const employeeRequestTimeOffApi = {};
+const employeeRequestTimeOffApi = {
+  // CREATE LEAVE
+  createLeave: (payload) => {
+    return axiosInstance.post(
+      "/employee/request-time-off/request",
+      payload
+    );
+  },
+
+  // GET HISTORY
+  getLeaveHistory: (status) => {
+    return axiosInstance.get(
+      `/employee/request-time-off/history${status ? `?status=${status}` : ""}`
+    );
+  },
+
+  // GET SUMMARY
+  getLeaveSummary: () => {
+    return axiosInstance.get(
+      "/employee/request-time-off/summary"
+    );
+  },
+};
 
 export default employeeRequestTimeOffApi;
+
