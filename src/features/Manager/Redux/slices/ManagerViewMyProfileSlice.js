b68d27bfdc790ee233ccb20fcde0f3fed446eaa8
Ashwini-patil18 <Ashwini.Patil@revappayyaitservices.com>
2026-02-19 15:25:12 +0530

    MangerViewMyProfile (Ashwini Patil)

--- src/features/Manager/Redux/slices/ManagerViewMyProfileSlice.js
+++ src/features/Manager/Redux/slices/ManagerViewMyProfileSlice.js
@@ -12,8 +12,26 @@ const managerViewMyProfileSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        builder;
+        builder
+            // Pending
+            .addCase(managerViewMyProfileThunk.viewMyProfile.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+
+            // Fulfilled
+            .addCase(managerViewMyProfileThunk.viewMyProfile.fulfilled, (state, action) => {
+                state.loading = false;
+                state.data = action.payload;
+            })
+
+            // Rejected
+            .addCase(managerViewMyProfileThunk.viewMyProfile.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
     },
 });
 
 export default managerViewMyProfileSlice.reducer;
+ 
--- src/features/Manager/Redux/thunks/ManagerViewMyProfileThunk.js
+++ src/features/Manager/Redux/thunks/ManagerViewMyProfileThunk.js
@@ -3,4 +3,19 @@ import managerViewMyProfileApi from "../../../../api/managerApi/ManagerViewMyPro
 
 const managerViewMyProfileThunk = {};
 
+// View My Profile Thunk
+managerViewMyProfileThunk.viewMyProfile = createAsyncThunk(
+    "manager/viewMyProfile",
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await managerViewMyProfileApi.viewMyProfile();
+            return response;
+        } catch (error) {
+            return rejectWithValue(
+                error.response?.data || "Something went wrong"
+            );
+        }
+    }
+);
+
 export default managerViewMyProfileThunk;
