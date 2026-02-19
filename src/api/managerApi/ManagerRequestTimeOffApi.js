acf145a5ace2c5463c4907f4d7c11cd1f4fd6132
swati826 <swati.ghante@revappayyaitservices.com>
2026-02-19 14:17:25 +0530

    i added Manager Request Time Off API integration

--- src/api/managerApi/ManagerRequestTimeOffApi.js
+++ src/api/managerApi/ManagerRequestTimeOffApi.js
@@ -1,5 +1,27 @@
+// import axiosInstance from "../axiosInstance";
+
+// const managerRequestTimeOffApi = {};
+
+// export default managerRequestTimeOffApi;
+
+
 import axiosInstance from "../axiosInstance";
 
-const managerRequestTimeOffApi = {};
+const managerRequestTimeOffApi = {
+  // CREATE LEAVE
+  createLeave: (data) =>
+    axiosInstance.post("/manager/request-time-off", data),
+
+  // GET HISTORY
+  getHistory: (status) =>
+    axiosInstance.get("/manager/request-time-off/history", {
+      params: status ? { status } : {},
+    }),
+
+  // GET SUMMARY
+  getSummary: () =>
+    axiosInstance.get("/manager/request-time-off/summary"),
+};
 
 export default managerRequestTimeOffApi;
+
