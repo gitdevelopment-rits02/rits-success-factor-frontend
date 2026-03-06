ddcd4748e18319675d43212dcbd967060ef65834
Parmeshwarrit <Parmeshwar.nagapalle@revappayyaitservices.com>
2026-03-06 18:23:11 +0530

    the login error is resolved

--- src/routes/groups/SuperAdminRoutesGroup.jsx
+++ src/routes/groups/SuperAdminRoutesGroup.jsx
@@ -4,12 +4,18 @@ import AdminLayout from "../../layouts/adminLayout";
 import { superAdminRoutes } from "../modules/superadmin.routes";
 import { store } from "../../app/store";
 import superAdminRootReducer from "../../features/SuperAdmin/Redux/superadminRootReducer";
+import PageLoader from "../../components/PageLoader";
 
 export default function SuperAdminRoutesGroup() {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
     useEffect(() => {
         store.reducerManager.add("superAdmin", superAdminRootReducer);
         console.log("SuperAdmin Reducer Dynamic Injection: SUCCESS");
+        setIsLoaded(true);
     }, []);
+
+    if (!isLoaded) return <PageLoader />;
 
     return (
         <Routes>
